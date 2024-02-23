f0c1f32512d1de5ee76f3999dd913fb3a35ff00c
Jeffrey LaFleur <99512448+jlafleur9@users.noreply.github.com>
2024-02-22 17:48:52 -0700

    Time: 8559 ms (5.00%), Space: 324.5 MB (5.00%) - LeetHub

--- 0204-count-primes/0204-count-primes.py
+++ 0204-count-primes/0204-count-primes.py
@@ -20,26 +20,21 @@ class Solution:
                 value += 2
                 nonprimes.add(value)
         
-                
-        
-        
         for num in range(3,n):
             if num % 2 == 0:  
                 continue
             if num not in nonprimes:
                 primes += 1
                 value = num * 3
-                nonprimes.add(value)
                 while value < n:
-                    value += num * 2
                     nonprimes.add(value)
+                    value += num * 2
                 continue
             if num in nonprimes:
                 value = num * 3
-                nonprimes.add(value)
                 while value < n:
-                    value += num * 2
                     nonprimes.add(value)
+                    value += num * 2
         
         return primes
                 
