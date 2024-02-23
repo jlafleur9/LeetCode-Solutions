4534c326cdeb3b40fdec0d1c7e02de46b9a27c31
Jeffrey LaFleur <99512448+jlafleur9@users.noreply.github.com>
2024-02-22 17:46:21 -0700

    Time: 9159 ms (5.00%), Space: 598.9 MB (5.00%) - LeetHub

--- 0204-count-primes/0204-count-primes.py
+++ 0204-count-primes/0204-count-primes.py
@@ -2,26 +2,43 @@ class Solution:
     def countPrimes(self, n: int) -> int:
         primes = 0
         
-        if n == 0 or n == 1:
+        if n == 0 or n == 1 or n == 2:
             return primes
         
         nonprimes = set()
         
-        for num in range(2,n):
-            if num not in nonprimes:
-                primes += 1
-                value = num + num
+        if n == 3:
+            primes += 1
+            return primes
+        
+        #add 2 to primes and eliminate all even numbers. 
+        if n > 3:
+            primes += 1
+            value = 4
+            nonprimes.add(value)
+            while value < n:
+                value += 2
                 nonprimes.add(value)
-                while value < n:
-                    value += num
-                    nonprimes.add(value)
-                continue
-            #just skip even numbers 2 will always run through first if statement and add all even numbers will be added to nonprimes
+        
+                
+        
+        
+        for num in range(3,n):
             if num % 2 == 0:  
                 continue
-            if num in nonprimes:
+            if num not in nonprimes:
+                primes += 1
+                value = num * 3
+                nonprimes.add(value)
                 while value < n:
-                    value += num
+                    value += num * 2
+                    nonprimes.add(value)
+                continue
+            if num in nonprimes:
+                value = num * 3
+                nonprimes.add(value)
+                while value < n:
+                    value += num * 2
                     nonprimes.add(value)
         
         return primes
