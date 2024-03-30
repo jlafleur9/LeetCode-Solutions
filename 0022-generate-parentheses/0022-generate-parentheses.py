5b714bd239ddf014e1374dec1919039551c08f00
Jeffrey LaFleur <99512448+jlafleur9@users.noreply.github.com>
2024-03-30 12:25:53 -0700

    Time: 35 ms (76.36%), Space: 16.7 MB (89.54%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -2,7 +2,7 @@ class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         stack = []
         ans = []
-        currentString = ""
+        
         
         def recursive(openP, closedP):
             if openP == closedP == n:
