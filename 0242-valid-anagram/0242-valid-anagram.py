371d543bd3d9e88168e424303dafad886578c439
Jeffrey LaFleur <99512448+jlafleur9@users.noreply.github.com>
2024-04-28 05:34:27 -0700

    Time: 55 ms (28.22%), Space: 16.9 MB (64.73%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -20,36 +20,3 @@ class Solution:
             
         return True
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        char_map2 = {}
-
-        if len(s) != len(t):
-            return False
-
-        for char in s:
-            if char in char_map:
-                char_map[char] += 1
-            else:
-                char_map[char] = 1
-        
-        for char in t:
-            if char in char_map and char_map[char] !=0:
-                char_map[char] -= 1
-            else:
-                return False
-        return True
