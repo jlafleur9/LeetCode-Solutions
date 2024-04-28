7d1b4288bca8c4ef2bf9b749db4d22bef706ba9e
Jeffrey LaFleur <99512448+jlafleur9@users.noreply.github.com>
2024-04-28 05:34:14 -0700

    Time: 58 ms (16.33%), Space: 16.9 MB (64.73%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,55 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        char_map = {}
+        
+        if len(s) != len(t):
+            return False
+        
+        
+        for char in s:
+            if char in char_map:
+                char_map[char] += 1
+            else:
+                char_map[char] = 1
+        
+        for char in t:
+            if char in char_map and char_map[char] != 0:
+                char_map[char] -= 1
+            else:
+                return False
+            
+        return True
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        char_map = {}
+
+        if len(s) != len(t):
+            return False
+
+        for char in s:
+            if char in char_map:
+                char_map[char] += 1
+            else:
+                char_map[char] = 1
+        
+        for char in t:
+            if char in char_map and char_map[char] !=0:
+                char_map[char] -= 1
+            else:
+                return False
+        return True
