1197ea6299afe878c93bcf7b80f0c3f100b4348e
Jeffrey LaFleur <99512448+jlafleur9@users.noreply.github.com>
2024-05-01 23:40:59 -0700

    Time: 52 ms (6.33%), Space: 16.8 MB (16.85%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
@@ -1,8 +1,6 @@
 class Solution:
     def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
         count = 0
-        print(students)
-        print(sandwiches)
         while count < len(students):
             if len(sandwiches) == 0:
                 break
@@ -11,7 +9,6 @@ class Solution:
                 sandwiches.pop(0)
                 count = 0
             else:
-                print(students)
                 students.append(students.pop(0))
                 count += 1
                 
