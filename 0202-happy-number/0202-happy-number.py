4468d3effb1a68ab7ae2756b2f039be2d65f2b2d
Jeffrey LaFleur <99512448+jlafleur9@users.noreply.github.com>
2024-05-02 03:30:44 -0700

    Time: 42 ms (23.07%), Space: 16.4 MB (80.72%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        cycle = set()
+        
+        if n == 1:
+            return True
+        
+        
+            
+        while n not in cycle:
+            cycle.add(n)
+            digits = str(n)
+            n = sum(int(digit)**2 for digit in digits)
+            if n == 1:
+                return True
+        
+        return False
+            
+            
+            
